77398d99ce53a71be4d9c1924b46c05b34ceb06c
Vu Nguyen <vu@vnguyen.io>
2016-07-07 21:54:35 +0000

    part 1-3 completed

--- rottenpotatoes/features/sort_movie_list.feature
+++ rottenpotatoes/features/sort_movie_list.feature
@@ -23,8 +23,10 @@ Background: movies have been added to database
 
 Scenario: sort movies alphabetically
   When I follow "Movie Title"
-  # your steps here
+  Then I should see "Aladdin" before "Chocolat"
+  Then I should see "Chicken Run" before "The Help"
 
 Scenario: sort movies in increasing order of release date
   When I follow "Release Date"
-  # your steps here
+  Then I should see "When Harry Met Sally" before "Chicken Run"
+  Then I should see "Chicken Run" before "The Help"
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -14,7 +14,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  expect(page.body).to match(/#{e1}.*#{e2}/m)
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,10 +25,22 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  
+  rating_list.split(', ').each {|x|
+    if uncheck == "un"
+      step %{I uncheck "ratings_#{x}"}
+    else
+      step %{I check "ratings_#{x}"}
+    end
+  }
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  rows = all('//table/tbody/tr').count
+  rows.should == Movie.all.count
+  
+  Movie.all.each do |m|
+	  step %{I should see "#{m["title"]}"}
+  end
 end
